f10a2fd3bacdcfe74bc14616786f1ca1f9a9cad9
mubarakadetunji <47992535+mubarakadetunji@users.noreply.github.com>
2019-07-27 21:05:09 -0600

    I'm still working on this. Gonna be done real soon
    FootballZombie. Still working on it. Dw, I got it. Trust!

--- FootballZombie.java
+++ FootballZombie.java
@@ -10,11 +10,28 @@ public class FootballZombie implements Zombie{
     private double position;
     private Image zombie;
     private Image displayzombie;
+    private StackPane s = new StackPane();
+    private Group root = new Group();
+    private double ximage;
+    private double yimage;
 
     public FootballZombie(int row, double speed, double position){
         this.row = row;
         this.zombie = new Image("zombie_football.png");
         this.displayzombie = new ImageView(zombie);
-        
+        s.setTranslateY((int)((135+(row-1)*110+55)-40+1));
+        s.setTranslateX((int)(60+(column-1)*80+40)-40);
+        this.yimage = (135+(row-1)*110+55)-40;
+        this.ximage = (60+(column-1)*80+40)+40;     
     }
-}
+
+    /**
+     * Method for taking away the image of the FootballZombie
+     */
+
+     public void removeimage(){
+         this.s.getChildren().remove(this.displayzombie);
+         this.root.getChildren().remove(this.s);
+     }
+
+     
