5f68af19ef1c74d584bf090e01e1760a76372c13
greenjellyace <52971852+greenjellyace@users.noreply.github.com>
2019-07-30 03:36:24 -0600

    added comments

--- Menu.java
+++ Menu.java
@@ -7,16 +7,32 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 
+/**
+ * Menu class is an extension of BaseScene.
+ * It is the first scene to appear once the application is run.
+ *
+ */
 public class Menu extends BaseScene {
 
+	/**
+	 * Menu constructors. This passes a session to the parent (BaseScene).
+	 * @param aSession	session to be passed
+	 */
 	public Menu(Session aSession) {
 		super(aSession);
 	}
 
+	/**
+	 * Override abstract parent's (BaseScene) method to setup and display scene
+	 * (actual drawing in window).
+	 */
 	@Override
 	public void setup() throws Exception {
-		//Create buttons
-		Button start = new Button("start");	//launches second scene
+		/**
+		 * Create a start button that will launch the second scene (gardenScene)
+		 * once clicked.
+		 */
+		Button start = new Button("start");
 //		Button quit = new Button("quit");	//quits
 		
 		//Add buttons to hbox
@@ -25,18 +41,18 @@ public class Menu extends BaseScene {
 //		box.getChildren().add(quit);
 		box.setAlignment(Pos.CENTER);
 		
-		//Attach eventhandler
 /*		BtnQuitHandler qHandler = new BtnQuitHandler();
 		quit.setOnAction(qHandler);
 */		
+		// Attach start button handler to start button.
 		StartButtonHandler sHandler = new StartButtonHandler(getSession());
 		start.setOnAction(sHandler);
 		
-		//Add hbox to pane
+		//Add hbox with the button/s to pane
 		StackPane pane = new StackPane();
 		pane.getChildren().add(box);
 		
-		//Set the scene
+		//Set the scene with preferred dimensions.
 		setScene(new Scene(pane, 600, 500));
 		display();
 	}
