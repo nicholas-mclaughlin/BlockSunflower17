2cdfee015bbe24fe72db4b4d9deca9a0f0c5cb6e
greenjellyace <52971852+greenjellyace@users.noreply.github.com>
2019-07-30 03:03:03 -0600

    added comments

--- Session.java
+++ Session.java
@@ -2,29 +2,38 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-//This is a driver class which will just contain a main method
-
+/**
+ * This session class will be an extension of the class Application
+ * and will contain a main method.
+ * 
+ */
 public class Session extends Application {
+	
+	//Initialize stage for Application Structure.
 	private Stage stage;
 
-	//setter for application
+	/*
+	 * Sets scene to be initialized in stage
+	 * or upon running the application.
+	 * 
+	 * @param aScene    scene to be set
+	 */
 	public void setScene(Scene aScene) {
 		stage.setScene(aScene);
 	}
 
-	//override application method start()
+	/**
+	 * Override start() method in Application.
+	 * @param Stage	  stage to be 'drawn' upon running application.
+	 */
 	@Override
 	public void start(Stage aStage) throws Exception {
 		this.stage = aStage;
 		stage.setTitle("PvZ");
-		stage.setResizable(false);
+		stage.setResizable(false); //Window will not be resizable; images will not be moved
 		stage.show();
-
-		//start button (scene) omitted for now
-
-		//create the scene for gameplay
-		//GardenScene scene = new GardenScene(this);
-		//Show start/menu as first scene
+		
+		//Create new first scene object upon running application.
 		Menu scene = new Menu(this);
 		scene.setup(); //setup scene for stage (method is in class gardenscene)
 	}
