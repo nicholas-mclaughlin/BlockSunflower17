9d58539450f8c5d043071e903187fab5efce6445
greenjellyace <52971852+greenjellyace@users.noreply.github.com>
2019-07-30 03:46:43 -0600

    added comments

--- Player.java
+++ Player.java
@@ -1,10 +1,19 @@
 //package logic;
 
+/**
+ * Player class is responsible for keeping track of the player's
+ * money and plant.
+ */
 public class Player {
+	/**
+	 * The default start amount of money is 50 suns.
+	 * plantHeld will keep track of which plant did the player pick
+	 */
 //	private String name;
 	private int money = 50;
 	private String plantHeld;
 	
+	//constructor
 	public Player(int aMoney, String aPlant) {
 		this.money = aMoney;
 		this.plantHeld = aPlant;
@@ -22,10 +31,17 @@ public class Player {
 		this.money = money;
 	}
 
+	/**
+	 * getPlantHeld returns the type of plant picked by the player
+	 * by clicking the plant's button on the gardescene
+	 * 
+	 * @return	new String object of the type of plant
+	 */
 	public String getPlantHeld() {
 		return new String(this.plantHeld);
 	}
 
+	//plantHeld setter
 	public void setPlantHeld(String aPlant) {
 		this.plantHeld = aPlant;
 		System.out.println(getPlantHeld());
