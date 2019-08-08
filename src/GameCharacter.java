84c6ecc2ee038a9de37ab6597aa1868328fd9093
mubarakadetunji <47992535+mubarakadetunji@users.noreply.github.com>
2019-08-07 19:21:53 -0600

    making comments clearer

--- src/GameCharacter.java
+++ src/GameCharacter.java
@@ -1,6 +1,6 @@
 //package logic;
 
-//Class that has all the attributes that all character will inherit from
+// This class has all the attributes the Plants and Zombies will inherit from.
 public class GameCharacter {
 	private String type = " "; //The type of character ie. PeaShooter
 	private int health; //Health of character
